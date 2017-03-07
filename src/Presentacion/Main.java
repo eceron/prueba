f0d3bc035fd89ed5a8e18ec44093ea7ea37681be
eceron <eceron893@gmail.com>
2017-03-06 19:40:54 -0500

    Update Main.java

--- src/Presentacion/Main.java
+++ src/Presentacion/Main.java
@@ -18,7 +18,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo hola mundo");
     }
     
 }
